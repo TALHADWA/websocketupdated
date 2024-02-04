2884d220336774928bc3443c97bf4a2e1321cd7d
talha <talhaali21cv@gmail.com>
2024-02-04 02:57:16 -0800

    updated

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-const express=require("express");
-const cors=require("cors");
-const mongoose=require("mongoose");
-const expresss=express();
-const http=require("http");
-const port= process.env.port || 4545 ;
-var server= http.createServer(expresss);
-const socket=require("socket.io")(server);
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const expresss = express();
+const http = require("http");
+const port = process.env.port || 4545;
+var server = http.createServer(expresss);
+const socket = require("socket.io")(server);
 expresss.use(express.json());
-// expresss.use(cors());
+
+var signin = {};
 
 socket.on('connection', (socket) => {
   console.log("fff");
   console.log(socket.id);
-  socket.on("/fuck", (msg)=>{
-    console.log(msg);
-    socket.emit("/msg", msg);
- })
- });
- 
 
+  socket.on("/signin", (sockets) => {
+    signin[sockets] = { email: sockets, socket: socket };
+    console.log("Stored signin data for socket ID", sockets, ":", signin[sockets]);
+  });
+  
 
-server.listen(port,()=>{
-console.log(port);
-})
+  socket.on("/fuck", (msg) => {
+    let checkrecipent = msg["to"];
+    console.log(signin);
+    console.log("Recipient ID:", checkrecipent);
+    if(signin[checkrecipent]
+      )
+      {
+        console.log("contains");
+      }
+      else{
+        console.log("not contains");
+      }
+    // for (const check in signin) {
+    //   console.log(check);
+    //   if (signin[check] === checkrecipent) {
+    //     signin[check].socket.emit("/messg", msg);
+    //   } else {
+    //     console.log("not found");
+    //   }
+    // }
+  });
+});
 
-
-
-
-
-
-
-
-
+server.listen(port, () => {
+  console.log(port);
+});
